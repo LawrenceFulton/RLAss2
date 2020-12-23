9b0e50e0a8c3837cc5be3777364cab8b54d2055c
LawrenceFulton <l.b.fulton@student.rug.nl>
2020-12-23 16:50:11 +0100

    added main file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+int main(int argc, char const *argv[])
+{
+  Cat *cat = new Cat(1,2,3);
+  cat->printCoordinates();
+  return 0;
+}
+
+
+
---
+++ main.h
@@ -0,0 +1,7 @@
+#include "code/header/Agent.h"
+#include "code/header/Cat.h"
+#include "code/header/Mouse.h"
+#include "code/header/World.h"
+
+
+
