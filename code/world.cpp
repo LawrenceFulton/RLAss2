5188b8b46a096e5b14d05674f752e566f4f32dc4
LawrenceFulton <l.b.fulton@student.rug.nl>
2020-12-24 14:12:03 +0100

    made outside walls into the world

--- code/world.cpp
+++ code/world.cpp
@@ -12,7 +12,31 @@ World::World(int sizeX, int sizeY){
   {
     locations[i] = new Location[sizeY];
   }
+  //Making the boarders of the world, no walls (for now)
+  
+  // top and bottom boarder of the world 
+  for (size_t x = 0; x < sizeX; x++)
+  { 
+    locations[x][0].setPossDirect(true, true, true, false);
+    locations[x][sizeY-1].setPossDirect(true,true, false, true);
+  }
+
+  // left and right boarder of the world 
+  for (size_t y = 0; y < sizeY; y++)
+  {
+    locations[0][y].setPossDirect(false, true, true, true);
+    locations[sizeX-1][y].setPossDirect(true,false,true,true);
+  }
+
+  //corner points
+
+  locations[0][0].setPossDirect(0,1,1,0);
+  locations[0][sizeY-1].setPossDirect(0,1,0,1);
+  locations[sizeX-1][sizeY-1].setPossDirect(1,0,0,1);
+  locations[sizeX-1][0].setPossDirect(1,0,1,0);
 }
+
+
 void World::printAllPossDirect(){
   for (size_t x = 0; x < sizeX; x++)
   {
