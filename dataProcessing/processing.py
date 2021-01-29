81a60411214ea12713f14981ff97f8b679bdc5e7
LawrenceFulton <l.b.fulton@student.rug.nl>
2021-01-29 09:43:21 +0100

    attepting to merge everything into main

--- dataProcessing/processing.py
+++ dataProcessing/processing.py
@@ -1,7 +1,11 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 data = np.genfromtxt("./data/reward.txt", delimiter=',',)
 
+data = np.cumsum(data) / np.arange(1, len(data)+1)
 
+
+plt.plot(data)
+plt.savefig("figures/dsada")
 
 print(data)
